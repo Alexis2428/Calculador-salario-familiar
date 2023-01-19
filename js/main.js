54af7a4766fed620e349aee0d961fae9d14bfdcd
Alexis Landa <114886294+Alexis2428@users.noreply.github.com>
2023-01-19 19:20:11 -0300

    Agrega funciones para agregar o quitar objetos

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
+function agregarIntegrante() {
+    const $cuadroTexto = document.createElement('input');
+    $cuadroTexto.type = 'number';
+    $cuadroTexto.className = 'form-control';
+
+    const $texto = document.createElement('label');
+    $texto.textContent = 'Ingrese el salario anual';
+
+    const $integrante = document.createElement('div');
+    $integrante.classList.add('form-floating');
+    $integrante.classList.add('mb-1');
+    $integrante.classList.add('integrante');
+
+    $integrante.appendChild($cuadroTexto);
+    $integrante.appendChild($texto);
+
+    document.querySelector('#integrantes').appendChild($integrante);
+}
+
+function borrarUltimoIntegrante() {
+    const $integrantes = document.querySelectorAll('.integrante');
+    if (0 < $integrantes.length) {
+        const ultimoIntegrante = $integrantes.length - 1;
+        $integrantes[ultimoIntegrante].remove();
+    }
+}
+
+function borrarIntegrantes() {
+    const $integrantes = document.querySelectorAll('.integrante');
+    for (let i = $integrantes.length; i > 0; i--) {
+        borrarUltimoIntegrante();
+    }
+}
+
 function mostrarBotonCalcular() {
     document.querySelector('button[name=calcular]').classList.remove('oculto');
 }
